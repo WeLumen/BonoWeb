e3c15c914aa70f58c73da85f3b57ca29d89c3a23
KathllynSantos <kathllyn.leticia@gmail.com>
2025-04-25 20:51:32 -0300

    Atualizações da pagina de privacidade

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.querySelector('.menu-toggle');
-  const closeBtn = document.querySelector('.close-menu');  // Botão de fechar (X)
-  const navLinks = document.querySelector('.nav-links');
-  const links = document.querySelectorAll('.nav-links a');
+// JS
+const menuToggle = document.querySelector('.menu-toggle');
+const closeMenu = document.querySelector('.close-menu');
+const navLinks = document.querySelector('.nav-links');
 
-  // Abrir o menu ao clicar no ícone hamburguer
-  toggle.addEventListener('click', () => {
-    navLinks.classList.add('show');
-    closeBtn.classList.add('show');  // Mostrar o botão de fechar
-  });
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.add('show');
+  menuToggle.classList.add('hide'); // Alternar com uma classe nova para esconder
+  closeMenu.classList.add('show');
+});
 
-  // Fechar o menu ao clicar no ícone X
-  closeBtn.addEventListener('click', () => {
-    navLinks.classList.remove('show');
-    closeBtn.classList.remove('show');  // Esconder o botão de fechar
-  });
-
-  // Fechar o menu ao clicar em qualquer link
-  links.forEach(link => {
-    link.addEventListener('click', () => {
-      navLinks.classList.remove('show');
-      closeBtn.classList.remove('show');  // Esconder o botão de fechar
-    });
-  });
+closeMenu.addEventListener('click', () => {
+  navLinks.classList.remove('show');
+  menuToggle.classList.remove('hide');
+  closeMenu.classList.remove('show');
 });
